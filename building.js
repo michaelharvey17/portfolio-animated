2b7e5f8bcd8e35f558ee3955a117f03b8179996a
Michael Harvey <michaelharvey17@gmail.com>
2015-08-28 14:18:13 -0400

    shortened timing on back animations.

--- building.js
+++ building.js
@@ -116,54 +116,54 @@ function buildingClick() {
 function buildingUnclick() {
   $('#trunk1').animate({
       left: "+=30em"
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#trunk2').animate({
     right: "+=30em"
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#branch').animate({
     right: "+=40em"
-  }, 5000, function(){
+  }, 3000, function(){
   });
   $('.leaf-wrap').animate({
     right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('.leaf-select').animate({
       right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('.leaf-stem').animate({
     right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#ground').animate({
     height: "+=45em",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#g-highlight1').animate({
     height: "+=45em",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#fire').animate({
     left: "+=20em",
     top: "-=20em",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#fire1, #fire2, #fire3, #fire4, #fire5, #fire6').animate({
       height: "-=2em",
       width: "-=2em",
       left: "-=.1em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
     $('#log').animate({
       height: "-=.5em",
       width: "-=2.5em",
       top: "-=40%"
-    }, 5000, function(){
+    }, 3000, function(){
     });
-  $('#fire-text').delay(5000).animate({
+  $('#fire-text').delay(3000).animate({
     opacity: "1",
     }, 1000, function(){
   });
@@ -172,46 +172,46 @@ function buildingUnclick() {
     top: "-=25em",
     height: "-=6em",
     width: "-=9em",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#support').animate({
     top: "-=20em",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#board h1').animate({
     fontSize: "-=1em",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#building').animate({
     height: "-=33em",
     width: "-=25em",
     top: "+=20%"
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#antenna').animate({
     height: "-=15em",
     width: "-=.75em",
     top: "+=40%",
   right: "-=15%",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#antenna-light').animate({
     height: "-=.75em",
     width: "-=.75em",
     top: "+=40%",
     right: "-=15%",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('#antenna-base').animate({
     height: "-=1.5em",
     width: "-=7em",
     top: "+=23.75%",
     right: "-=11%",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('.window').animate({
     borderWidth: "-=6px",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('.window').show("slow");
   $('.project').hide("slow");
@@ -220,7 +220,7 @@ function buildingUnclick() {
     fontSize: "-=2em",
     top: "+=30%",
     right: "-=7.5%",
-    }, 5000, function(){
+    }, 3000, function(){
   });
   $('sun, moon, .star').show();
   $('#building-back, #building-text').off('click');
--- fire.js
+++ fire.js
@@ -65,59 +65,59 @@ function fireClick() {
 function fireUnclick() {
   $('#trunk1').animate({
       left: "+=30em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#trunk2').animate({
       right: "+=30em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#branch').animate({
       right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('.leaf-wrap').animate({
       right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('.leaf-select').animate({
       right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('.leaf-stem').animate({
       right: "+=40em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#ground').animate({
       height: "-=50em",
       width: "-=50em",
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#g-highlight1').animate({
       height: "-=20em",
       width: "-=20em",
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#fire').animate({
       top: "30em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#fire1, #fire2, #fire3, #fire4, #fire5, #fire6').animate({
       height: "-=2em",
       width: "-=2em",
       left: "-=.1em"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#log').animate({
       height: "-=.5em",
       width: "-=2.5em",
       top: "-=40%"
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('#fire-text').stop().animate({
       left: "-=2%",
       top: "+=20.5em",
       fontSize: "1.25em",
-    }, 5000, function(){
+    }, 3000, function(){
     });
   $('sun, moon, .star').show();
   $('#social').css('visibility', 'hidden');
